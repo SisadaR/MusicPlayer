27f702e97b9a8182310c531e5ef9576b44168608
Sisada R <kong.sisada@gmail.com>
2021-01-30 20:45:39 +0700

    update seekbar position and text

--- app/src/main/java/com/jkhome/musicplayer/MusicPlayerActivity.kt
+++ app/src/main/java/com/jkhome/musicplayer/MusicPlayerActivity.kt
@@ -1,5 +1,6 @@
 package com.jkhome.musicplayer
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.pm.PackageManager
 import android.database.Cursor
@@ -9,10 +10,13 @@ import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.provider.MediaStore
 import android.util.Log
 import android.webkit.MimeTypeMap
 import android.widget.MediaController
+import android.widget.SeekBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -22,6 +26,7 @@ import com.CodeBoy.MediaFacer.MediaFacer
 import com.CodeBoy.MediaFacer.mediaHolders.audioContent
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.lang.StringBuilder
+import java.util.concurrent.TimeUnit
 
 class MusicPlayerActivity : AppCompatActivity() , ItemCLicked {
 
@@ -47,7 +52,7 @@ class MusicPlayerActivity : AppCompatActivity() , ItemCLicked {
         }
     }
 
-    fun play(currentPosition: Int)
+    private fun play(currentPosition: Int)
     {
         if(!state){
             findViewById<FloatingActionButton>(R.id.fab_play).setImageDrawable( resources.getDrawable(R.drawable.ic_stop))
@@ -64,6 +69,38 @@ class MusicPlayerActivity : AppCompatActivity() , ItemCLicked {
             mediaPlayer!!.stop()
             findViewById<FloatingActionButton>(R.id.fab_play).setImageDrawable( resources.getDrawable(R.drawable.ic_play_arrow))
         }
+
+        val mHandler = Handler()
+        this@MusicPlayerActivity.runOnUiThread(object : Runnable{
+            override fun run() {
+                val playerPosition = mediaPlayer?.currentPosition!! / 1000
+                val totalDuration = mediaPlayer?.duration!! / 1000
+
+                val seekBar = findViewById<SeekBar>(R.id.seek_bar)
+                seekBar.max = totalDuration
+                seekBar.progress = playerPosition
+
+                val pastTextView = findViewById<TextView>(R.id.past_text_view)
+                pastTextView.text = timerFormat( playerPosition.toLong())
+
+                val remainTextView = findViewById<TextView>(R.id.remain_text_view)
+                remainTextView.text = timerFormat(totalDuration - playerPosition.toLong())
+
+                mHandler.postDelayed(this,1000)
+            }
+
+        })
+    }
+
+    fun timerFormat(time:Long):String{
+        val result = String.format("%02d:%02d",
+                TimeUnit.SECONDS.toMinutes(time),
+                TimeUnit.SECONDS.toSeconds(time) - TimeUnit.MINUTES.toSeconds(TimeUnit.SECONDS.toMinutes(time)))
+        var convert = ""
+        for(element in result)
+            convert += element
+
+        return convert
     }
 
     private fun getSongsUsingLibMediaFacer(){
@@ -91,6 +128,7 @@ class MusicPlayerActivity : AppCompatActivity() , ItemCLicked {
 
         return;
     }
+
     private fun getSongs(){
         val selection = MediaStore.Audio.Media.IS_MUSIC + "=1";
         //val sortOrder = MediaStore.Audio.Media.TITLE + " ASC"
